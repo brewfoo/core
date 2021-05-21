8fbd78d20ad4f44b8ef4899ff6b60ea646af58fc
Dimitrij Denissenko <dimitrij.denissenko@blacksquaremedia.com>
2021-05-21 11:53:49 +0100

    Improve hop stand calculations

--- Makefile
+++ Makefile
@@ -1,21 +1,32 @@
 NPM_BIN=./node_modules/.bin
 
-default: lint test
-all: default build
+default: test
+all: default rebuild doc
 
-lint:
+test: test.lint test.format test.unit
+test.lint:
 	$(NPM_BIN)/eslint 'src/**/*.{ts,js}'
-
-test:
+test.format:
+	$(NPM_BIN)/prettier --list-different 'src/**/*.{ts,js}'
+test.unit:
 	$(NPM_BIN)/jest
 
-prettify:
-	$(NPM_BIN)/prettier --write --config prettier.config.js 'src/**/*.{ts,js}'
+fix: fix.lint fix.format
+fix.lint:
+	$(NPM_BIN)/eslint --fix 'src/**/*.{ts,js}'
+fix.format:
+	$(NPM_BIN)/prettier --write 'src/**/*.{ts,js}'
 
+.PHONY: doc
+doc: doc/index.html
+doc/index.html: $(wildcard src/**)
+	$(NPM_BIN)/typedoc
+
+rebuild: clean build
+clean:
+	rm -rf lib/
 build: lib/esm/core.js lib/cjs/core.js
-
 lib/esm/core.js: tsconfig.json pnpm-lock.yaml $(wildcard src/**)
 	$(NPM_BIN)/tsc -p $<
-
 lib/cjs/core.js: tsconfig.cjs.json tsconfig.json pnpm-lock.yaml $(wildcard src/**)
 	$(NPM_BIN)/tsc -p $<
--- src/calc/utilization.test.ts
+++ src/calc/utilization.test.ts
@@ -5,68 +5,64 @@ import { UtilizationCalc } from './utilization';
 import { Duration } from '../quantity/duration';
 
 describe('UtilizationCalc', () => {
-  const boilHops = new UtilizationCalc({
+  const boilOnly = new UtilizationCalc({
     wortGravity: Gravity.points(60),
     batchVolume: Volume.liter(20),
     boilTime: Duration.minutes(60),
-    ambientTemperature: Temperature.celsius(20),
   });
-  const aromaStand = new UtilizationCalc({
+  const plusStand = new UtilizationCalc({
     wortGravity: Gravity.points(60),
     batchVolume: Volume.liter(20),
-    boilTime: Duration.minutes(10),
+    boilTime: Duration.minutes(50),
     standTime: Duration.minutes(10),
-    ambientTemperature: Temperature.celsius(20),
   });
   const flameOut = new UtilizationCalc({
     wortGravity: Gravity.points(60),
     batchVolume: Volume.liter(20),
     boilTime: Duration.none(),
     standTime: Duration.minutes(10),
-    ambientTemperature: Temperature.celsius(20),
   });
   const dryHop = new UtilizationCalc({
     wortGravity: Gravity.points(60),
     batchVolume: Volume.liter(20),
     boilTime: Duration.none(),
     standTime: Duration.none(),
-    ambientTemperature: Temperature.celsius(20),
   });
 
-  const setup = (
-    wg: number,
-    bv: number,
-    bt: number,
-    st?: number,
-    wt?: Temperature,
-    at?: Temperature,
-  ): UtilizationCalc => {
+  const setup = (opts: {
+    sg: number;
+    liter: number;
+    boil?: number;
+    stand?: number;
+    wort?: Temperature;
+    ambient?: Temperature;
+  }): UtilizationCalc => {
     return new UtilizationCalc({
-      wortGravity: new Gravity(GravityUnit.SG, wg),
-      batchVolume: Volume.liter(bv),
-      boilTime: Duration.minutes(bt),
-      standTime: st != null ? Duration.minutes(st) : undefined,
-      ambientTemperature: at ?? Temperature.celsius(20),
-      wortTemperature: wt,
+      wortGravity: new Gravity(GravityUnit.SG, opts.sg),
+      batchVolume: Volume.liter(opts.liter),
+      boilTime: Duration.minutes(opts.boil || 0),
+      standTime: opts.stand != null ? Duration.minutes(opts.stand) : undefined,
+      wortTemperature: opts.wort,
+      ambientTemperature: opts.ambient,
     });
   };
 
   it('should calculate rager', () => {
-    expect(boilHops.rager()).toBeCloseTo(29.4, 1);
-    expect(aromaStand.rager()).toBeCloseTo(9.1, 1);
+    expect(boilOnly.rager()).toBeCloseTo(29.4, 1);
+    expect(plusStand.rager()).toBeCloseTo(28.9, 1);
     expect(flameOut.rager()).toBeCloseTo(4.9, 1);
     expect(dryHop.rager()).toEqual(0.0);
   });
 
   it('should calculate tinseth', () => {
-    expect(boilHops.tinseth()).toBeCloseTo(21.1, 1);
-    expect(aromaStand.tinseth()).toBeCloseTo(11.5, 1);
+    expect(boilOnly.tinseth()).toBeCloseTo(21.1, 1);
+    expect(plusStand.tinseth()).toBeCloseTo(20.8, 1);
     expect(flameOut.tinseth()).toBeCloseTo(5.8, 1);
     expect(dryHop.tinseth()).toEqual(0.0);
   });
 
   it('should ignore invalid formulae', () => {
-    expect(boilHops.calculate(-1 as never)).toEqual(0.0);
+    expect(boilOnly.calculate(-1 as never)).toEqual(0.0);
   });
 
   it('should allow custom multiplier', () => {
@@ -74,40 +70,53 @@ describe('UtilizationCalc', () => {
       wortGravity: Gravity.points(60),
       batchVolume: Volume.liter(20),
       boilTime: Duration.minutes(60),
-      ambientTemperature: Temperature.celsius(20),
       multiplier: 1.1,
     });
     expect(calc.tinseth()).toBeCloseTo(23.2, 1);
   });
 
   it('should adjust for gravity', () => {
-    expect(setup(1.06, 20, 60, 0).tinseth()).toBeCloseTo(21.1, 1);
-    expect(setup(1.04, 20, 60, 0).tinseth()).toBeCloseTo(25.2, 1);
+    expect(setup({ sg: 1.06, liter: 20, boil: 60 }).tinseth()).toBeCloseTo(21.1, 1);
+    expect(setup({ sg: 1.04, liter: 20, boil: 60 }).tinseth()).toBeCloseTo(25.2, 1);
 
-    expect(setup(1.06, 20, 10, 10).tinseth()).toBeCloseTo(11.5, 1);
-    expect(setup(1.04, 20, 10, 10).tinseth()).toBeCloseTo(13.8, 1);
+    expect(setup({ sg: 1.06, liter: 20, boil: 10, stand: 10 }).tinseth()).toBeCloseTo(11.5, 1);
+    expect(setup({ sg: 1.04, liter: 20, boil: 10, stand: 10 }).tinseth()).toBeCloseTo(13.8, 1);
 
-    expect(setup(1.06, 20, 0, 20).tinseth()).toBeCloseTo(7.5, 1);
-    expect(setup(1.04, 20, 0, 20).tinseth()).toBeCloseTo(9.0, 1);
+    expect(setup({ sg: 1.06, liter: 20, stand: 20 }).tinseth()).toBeCloseTo(7.5, 1);
+    expect(setup({ sg: 1.04, liter: 20, stand: 20 }).tinseth()).toBeCloseTo(9.0, 1);
   });
 
   it('should adjust for volume (on stands)', () => {
-    expect(setup(1.06, 20, 60, 0).tinseth()).toBeCloseTo(21.1, 1);
-    expect(setup(1.06, 10, 60, 0).tinseth()).toBeCloseTo(21.1, 1);
+    expect(setup({ sg: 1.06, liter: 20, boil: 60 }).tinseth()).toBeCloseTo(21.1, 1);
+    expect(setup({ sg: 1.06, liter: 10, boil: 60 }).tinseth()).toBeCloseTo(21.1, 1);
 
-    expect(setup(1.06, 20, 10, 10).tinseth()).toBeCloseTo(11.5, 1);
-    expect(setup(1.06, 10, 10, 10).tinseth()).toBeCloseTo(11.4, 1);
+    expect(setup({ sg: 1.06, liter: 20, boil: 10, stand: 10 }).tinseth()).toBeCloseTo(11.5, 1);
+    expect(setup({ sg: 1.06, liter: 10, boil: 10, stand: 10 }).tinseth()).toBeCloseTo(11.4, 1);
 
-    expect(setup(1.06, 20, 0, 20).tinseth()).toBeCloseTo(7.5, 1);
-    expect(setup(1.06, 10, 0, 20).tinseth()).toBeCloseTo(6.9, 1);
+    expect(setup({ sg: 1.06, liter: 20, stand: 20 }).tinseth()).toBeCloseTo(7.5, 1);
+    expect(setup({ sg: 1.06, liter: 10, stand: 20 }).tinseth()).toBeCloseTo(6.9, 1);
   });
 
-  it('should adjust for temperatures (on stands)', () => {
+  it('should adjust for wort temperatures (on stands)', () => {
     const T90 = Temperature.celsius(90);
+    const T70 = Temperature.celsius(70);
+
+    expect(setup({ sg: 1.06, liter: 20, stand: 20 }).tinseth()).toBeCloseTo(7.5, 1);
+    expect(setup({ sg: 1.06, liter: 20, stand: 20, wort: T90 }).tinseth()).toBeCloseTo(6.3, 1);
+    expect(setup({ sg: 1.06, liter: 20, stand: 20, wort: T70 }).tinseth()).toBeCloseTo(1.3, 1);
+  });
+
+  it('should adjust for ambient temperatures (on stands)', () => {
+    const T80 = Temperature.celsius(80);
     const T10 = Temperature.celsius(10);
 
-    expect(setup(1.06, 20, 0, 20).tinseth()).toBeCloseTo(7.5, 1);
-    expect(setup(1.06, 20, 0, 20, T90).tinseth()).toBeCloseTo(3.8, 1);
-    expect(setup(1.06, 20, 0, 20, T90, T10).tinseth()).toBeCloseTo(3.5, 1);
+    expect(setup({ sg: 1.06, liter: 20, stand: 20 }).tinseth()).toBeCloseTo(7.5, 1);
+    expect(setup({ sg: 1.06, liter: 20, stand: 20, ambient: T10 }).tinseth()).toBeCloseTo(7.0, 1);
+
+    // ignore ambient of wort temperature is given
+    expect(setup({ sg: 1.06, liter: 20, stand: 20, wort: T80 }).tinseth()).toBeCloseTo(2.9, 1);
+    expect(
+      setup({ sg: 1.06, liter: 20, stand: 20, wort: T80, ambient: T10 }).tinseth(),
+    ).toBeCloseTo(2.9, 1);
   });
 });
